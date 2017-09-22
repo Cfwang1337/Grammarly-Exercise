cda6a9f72b7b18ec51587a97b4d0c6d3bb8f5b45
Charles Wang <Cfwang1337@gmail.com>
2017-09-22 02:20:54 -0700

    First viz

--- read_csv.py
+++ read_csv.py
@@ -1,32 +1,94 @@
 from datetime import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import numpy as np
 import pandas as pd
+import seaborn as sns
 
-grammarly_df = pd.DataFrame.from_csv("grammarly_data_csv.csv")
 
-# print grammarly_df[:50]
+def main():
+    grammarly_df = pd.DataFrame.from_csv("grammarly_data_csv.csv")
+
+    print "0. DAILY ACTIVE USERS\n1. RETENTION CURVE\n2. EVALUATE SOURCES"
+    choice = raw_input("CHOOSE AN OPTION ")
+
+    while choice.lower() != "q" and choice in [str(x) for x in range(0, 3)]:
+        if choice == "0":
+            daily_active_users(grammarly_df)
+        elif choice == "1":
+            retention_curve(grammarly_df)
+        elif choice == "2":
+            evaluate_source(grammarly_df)
+        choice = raw_input("CHOOSE AN OPTION ")
+    exit()
+
 
 # COUNT DAILY ACTIVE USERS
-# grouped = grammarly_df.groupby('date')
-# user_count = grouped.uid.nunique()
-# print user_count
+def daily_active_users(grammarly_df):
 
-#TODO VISUALIZE DAILY ACTIVE USERS
+    grouped_by_date = grammarly_df.groupby('date')
+    user_count = pd.DataFrame(grouped_by_date.uid.nunique())
+    user_count.columns = ['count']
+    print user_count
+    sns.set_style("darkgrid")
+    x_axis = mdates.date2num([datetime.strptime(str(d).split(" ")[0], '%Y-%m-%d').date() for d in user_count.index])
+    y_axis = [float(y) for y in user_count['count']]
 
-print grammarly_df.index.dtype
-print grammarly_df.dtypes
-
-#TODO COUNT RETENTION CURVE
-
-feb_4 = grammarly_df[(grammarly_df.index == datetime.strptime('2016-02-04', '%Y-%m-%d')) & (grammarly_df['isFirst'] == True)]
-feb_4_cohort = list(set(feb_4['uid']))
-print feb_4
-
-
-#TODO VISUALIZE RETENTION CURVE
+    #TODO VISUALIZE DAILY ACTIVE USERS
+    plt.figure(figsize=(15, 8))
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+    plt.plot_date(x_axis, y_axis, 'b-')
+    plt.gcf().autofmt_xdate()
+    plt.show()
+    return
 
 
-#TODO EVALUATE BY TRAFFIC SOURCE
+# COUNT RETENTION CURVE
+def retention_curve(grammarly_df):
+    grouped_by_date = grammarly_df.groupby('date')
+    for feb_date in ["04", "10", "14"]:
+
+        date_to_format = '2016-02-{0}'.format(feb_date)
+
+        cohort_slice = grammarly_df[(grammarly_df.index == datetime.strptime(date_to_format, '%Y-%m-%d')) & (grammarly_df['isFirst'] == True)]
+        cohort_slice_cohort = list(set(cohort_slice['uid']))
+        # print cohort_slice
+        print "FEB {0} COHORT".format(feb_date), len(cohort_slice_cohort)
+
+        #TODO MAYBE THERE IS A MORE EFFICIENT WAY TO DO THIS
+
+        for name, group in grouped_by_date:
+            print name, group[group['uid'].isin(cohort_slice_cohort)].uid.nunique(), float(group[group['uid'].isin(cohort_slice_cohort)].uid.nunique())/float(len(cohort_slice_cohort))
+    return
+    #TODO VISUALIZE RETENTION CURVE
+
+
+#COUNT BY SOURCE
+def evaluate_source(grammarly_df):
+    grouped_by_source = grammarly_df.groupby('utmSource_cleaned')
+
+    total_users = grouped_by_source.uid.nunique()
+    print total_users.sort_values(ascending=False)
+
+    #AVERAGE DURATION OF VISITS
+    #NUMBER OF PINGS BY USER
+    for name, group in grouped_by_source:
+        grouped_by_uid = group.groupby(['uid', 'date'])
+        duration = []
+
+        # print name
+        for uid, uid_group in grouped_by_uid:
+            # print uid, max(uid_group['timestamp'].values) - min(uid_group['timestamp'].values)
+            duration.append((max(uid_group['timestamp'].values) - min(uid_group['timestamp'].values))/3600000.0)
+
+        print name, group.uid.nunique(), group.timestamp.nunique(), float(group.timestamp.nunique())/float(group.uid.nunique()), np.mean(duration)
+    return
+
+#TODO EQUALLY WEIGHT PINGS AND DURATION; MEASURE
+#TODO WHAT ABOUT AVERAGE TIME BETWEEN PINGS?
 
 #TODO VISUALIZE EVALUATION BY TRAFFIC SOURCE
 
-#TODO PREDICT SUCCESS BASED ON TIME OF DAY, SOURCE, AND ISFIRST
+if __name__ == "__main__":
+    main()
--- reformat_json.py
+++ reformat_json.py
@@ -2,24 +2,42 @@ import csv
 import json
 
 
-with open("grammarly_data_exercise.json", "rb") as myfile, open("grammarly_data_csv.csv", "wb") as writefile:
-    data = json.loads(myfile.read())
+def clean_sources(x):
+    if x is None:
+        return "other"
+    else:
+        return x.split('_')[0].split('.')[0].lower()
 
-    header = [
-        "date",
-        "timestamp",
-        "utmSource",
-        "uid",
-        "isFirst"
-    ]
 
-    writer = csv.DictWriter(writefile, fieldnames=header)
-    writer.writeheader()
-    entries = 0
-    for item in data:
-        entries += 1
-        result_dict = {}
-        for field in header:
-            result_dict[field] = item[field]
-        writer.writerow(result_dict)
-    print "LENGTH:", entries
+def main():
+    with open("grammarly_data_exercise.json", "rb") as myfile, open("grammarly_data_csv.csv", "wb") as writefile:
+        data = json.loads(myfile.read())
+
+        header = [
+            "date",
+            "timestamp",
+            "utmSource",
+            "utmSource_cleaned",
+            "uid",
+            "isFirst"
+        ]
+
+        writer = csv.DictWriter(writefile, fieldnames=header)
+        writer.writeheader()
+        entries = 0
+        for item in data:
+            entries += 1
+            result_dict = {}
+            for field in header:
+                if field != "utmSource_cleaned":
+                    result_dict[field] = item[field]
+
+                    if field == "utmSource":
+                        result_dict["utmSource_cleaned"] = clean_sources(item[field])
+
+            writer.writerow(result_dict)
+        print "LENGTH:", entries
+
+
+if __name__ == "__main__":
+    main()
