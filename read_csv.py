9e53684048886cefc81c588c258a993fee3ade64
Charles Wang <Cfwang1337@gmail.com>
2017-09-24 01:23:13 -0700

    No further work for now

--- read_csv.py
+++ read_csv.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from pandas import Timestamp
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import numpy as np
@@ -59,89 +60,95 @@ def daily_active_users(grammarly_df):
 # CALCULATE RETENTION CURVE
 def retention_curve(grammarly_df):
     grouped_by_date = grammarly_df.groupby('date')
+
+    date_index = list(set(grammarly_df.index))
+    date_index = sorted(date_index)
+
+    result_df = pd.DataFrame(columns=['date'])
+    result_df['date'] = date_index
+    result_df = result_df.set_index("date")
+
     for feb_date in ["04", "10", "14"]:
 
         date_to_format = '2016-02-{0}'.format(feb_date)
 
         cohort_slice = grammarly_df[(grammarly_df.index == datetime.strptime(date_to_format, '%Y-%m-%d')) & (grammarly_df['isFirst'] == True)]
         cohort_slice_cohort = list(set(cohort_slice['uid']))
-        # print cohort_slice
+
         print "FEB {0} COHORT".format(feb_date), len(cohort_slice_cohort)
 
-        result_set = []
+        sub_result_set = []
 
         for name, group in grouped_by_date:
             print name, group[group['uid'].isin(cohort_slice_cohort)].uid.nunique(), float(group[group['uid'].isin(cohort_slice_cohort)].uid.nunique())/float(len(cohort_slice_cohort))
             if group[group['uid'].isin(cohort_slice_cohort)].uid.nunique() > 0:
-                result_set.append([name, group[group['uid'].isin(cohort_slice_cohort)].uid.nunique(), float(group[group['uid'].isin(cohort_slice_cohort)].uid.nunique())/float(len(cohort_slice_cohort))])
+                sub_result_set.append([name, group[group['uid'].isin(cohort_slice_cohort)].uid.nunique(), float(group[group['uid'].isin(cohort_slice_cohort)].uid.nunique())/float(len(cohort_slice_cohort))])
 
-        result_df = pd.DataFrame(result_set, columns=[
+        sub_result_df = pd.DataFrame(sub_result_set, columns=[
             "date",
-            "uid_count",
-            "retention_rate"
+            "uid_count_{0}".format(feb_date),
+            "retention_rate_{0}".format(feb_date)
         ])
-        result_df = result_df.set_index("date")
+        sub_result_df = sub_result_df.set_index("date")
+        result_df = result_df.join(sub_result_df, how='left')
+    
+    result_df.to_csv("retention_curve_feb.csv")
 
-        result_df.to_csv("retention_curve_feb_{0}.csv".format(feb_date))
+    sns.set_style("darkgrid")
+    x_axis = mdates.date2num([datetime.strptime(str(d).split(" ")[0], '%Y-%m-%d').date() for d in result_df.index])
+    y_axis_1 = [float(y) for y in result_df['retention_rate_04']]
+    y_axis_2 = [float(y) for y in result_df['retention_rate_10']]
+    y_axis_3 = [float(y) for y in result_df['retention_rate_14']]
 
-        sns.set_style("darkgrid")
-        x_axis = mdates.date2num([datetime.strptime(str(d).split(" ")[0], '%Y-%m-%d').date() for d in result_df.index])
-        y_axis = [float(y) for y in result_df['retention_rate']]
+    #VISUALIZE DAILY ACTIVE USERS
 
-        #VISUALIZE DAILY ACTIVE USERS
+    fig = plt.figure(figsize=(15, 8))
+    ax = fig.add_subplot(111)
 
-        fig = plt.figure(figsize=(15, 8))
-        ax = fig.add_subplot(111)
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Retention rate")
 
-        ax.set_xlabel("Date")
-        ax.set_ylabel("Retention rate")
-
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-        plt.gca().xaxis.set_major_locator(mdates.DayLocator())
-        plt.plot_date(x_axis, y_axis, 'b-')
-        plt.gcf().autofmt_xdate()
-        plt.show()
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+    plt.plot_date(x_axis, y_axis_1, 'r-')
+    plt.plot_date(x_axis, y_axis_2, 'b-')
+    plt.plot_date(x_axis, y_axis_3, 'g-')
+    plt.gcf().autofmt_xdate()
+    plt.show()
 
     return
-    #TODO VISUALIZE RETENTION CURVE
 
 
 #COUNT BY SOURCE
-
 def evaluate_source(grammarly_df):
     grouped_by_source = grammarly_df.groupby('utmSource_cleaned')
 
     total_users = grouped_by_source.uid.nunique()
     print total_users.sort_values(ascending=False)
 
-    #AVERAGE DURATION OF VISITS
-    #NUMBER OF PINGS BY USER
-
     result_set = []
 
     for name, group in grouped_by_source:
-        # grouped_by_uid = group.groupby(['uid', 'date'])
-        # duration = []
-        # for uid, uid_group in grouped_by_uid:
-        #     duration.append((max(uid_group['timestamp'].values) - min(uid_group['timestamp'].values))/3600000.0)
+        grouped_by_uid = group.groupby(['uid', 'date'])
+        duration = []
+        for uid, uid_group in grouped_by_uid:
+            duration.append((max(uid_group['timestamp'].values) - min(uid_group['timestamp'].values))/3600000.0)
 
-        # print name, group.uid.nunique(), group.timestamp.nunique(), float(group.timestamp.nunique())/float(group.uid.nunique()), np.mean(duration)
-        # result_set.append([name, group.uid.nunique(), group.timestamp.nunique(), float(group.timestamp.nunique())/float(group.uid.nunique()), np.mean(duration)])
-        print name, group.uid.nunique(), group.timestamp.nunique(), float(group.timestamp.nunique())/float(group.uid.nunique())
-        result_set.append([name, group.uid.nunique(), group.timestamp.nunique(), float(group.timestamp.nunique())/float(group.uid.nunique())])
+        print name, group.uid.nunique(), group.timestamp.nunique(), float(group.timestamp.nunique())/float(group.uid.nunique()), np.mean(duration)
+        result_set.append([name, group.uid.nunique(), group.timestamp.nunique(), float(group.timestamp.nunique())/float(group.uid.nunique()), np.mean(duration)])
 
     result_df = pd.DataFrame(result_set, columns=[
         "Source",
         "UniqueUsers",
         "Pings",
         "PingsPerUser",
-        # "AverageDuration"
+        "AverageDuration"
     ])
 
     result_df = result_df.set_index("Source")
     result_df.to_csv("evaluate_source.csv")
 
-    for comparison in ["UniqueUsers", "Pings", "PingsPerUser"]:#, "AverageDuration"]:
+    for comparison in ["UniqueUsers", "Pings", "PingsPerUser", "AverageDuration"]:
         result_df = result_df.sort_values(comparison, ascending=True)
         sns.set_style("darkgrid")
 
@@ -163,13 +170,18 @@ def evaluate_source(grammarly_df):
 
         plt.show()
 
-        # exit()
-
     return
 
 
 #TODO VISUALIZE THIS
 def retention_rate_by_source(grammarly_df):
+
+    date_index = list(set(grammarly_df.index))
+    date_index = sorted(date_index)
+
+    result_df = pd.DataFrame(columns=['date'])
+    result_df['date'] = date_index
+    result_df = result_df.set_index("date")
 
     grouped_by_source = grammarly_df.groupby('utmSource_cleaned')
 
@@ -180,11 +192,67 @@ def retention_rate_by_source(grammarly_df):
 
         grouped_by_date = group.groupby('date')
         print name
-        for date, dategroup in grouped_by_date:
-            # print name, len(cohort_slice_cohort)
-            if len(cohort_slice_cohort) > 0:
+        if len(cohort_slice_cohort) > 3:
+            sub_result_set = []
+            for date, dategroup in grouped_by_date:
+                # print name, len(cohort_slice_cohort)
+
                 print date, dategroup[dategroup['uid'].isin(cohort_slice_cohort)].uid.nunique(), float(
                     dategroup[dategroup['uid'].isin(cohort_slice_cohort)].uid.nunique()) / float(len(cohort_slice_cohort))
+                sub_result_set.append([date, dategroup[dategroup['uid'].isin(cohort_slice_cohort)].uid.nunique(), float(
+                    dategroup[dategroup['uid'].isin(cohort_slice_cohort)].uid.nunique()) / float(len(cohort_slice_cohort))])
+
+            sub_result_df = pd.DataFrame(sub_result_set, columns=[
+                "date",
+                "count_{0}".format(name),
+                "retention_rate_{0}".format(name)
+            ])
+            sub_result_df = sub_result_df.set_index("date")
+            result_df = result_df.join(sub_result_df, how='left')
+
+    result_df.to_csv("retention_curve_by_source.csv")
+
+    sns.set_style("darkgrid")
+    x_axis = mdates.date2num([datetime.strptime(str(d).split(" ")[0], '%Y-%m-%d').date() for d in result_df.index])
+    y_axis_1 = [float(y) for y in result_df['retention_rate_answers']]
+    y_axis_2 = [float(y) for y in result_df['retention_rate_biznesowe+rewolucje']]
+    y_axis_3 = [float(y) for y in result_df['retention_rate_handbook']]
+    y_axis_4 = [float(y) for y in result_df['retention_rate_mosalingua+fr']]
+    y_axis_5 = [float(y) for y in result_df['retention_rate_other']]
+    y_axis_6 = [float(y) for y in result_df['retention_rate_program']]
+    y_axis_7 = [float(y) for y in result_df['retention_rate_shmoop_left']]
+    y_axis_8 = [float(y) for y in result_df['retention_rate_twitter']]
+
+    #VISUALIZE DAILY ACTIVE USERS
+
+    fig = plt.figure(figsize=(15, 8))
+    ax = fig.add_subplot(111)
+
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Retention rate")
+
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+    plt.plot_date(x_axis, y_axis_1, 'b-', label='answers')
+    plt.plot_date(x_axis, y_axis_2, 'g-', label='biznesowe+rewolucje')
+    plt.plot_date(x_axis, y_axis_3, 'r-', label='handbook')
+    plt.plot_date(x_axis, y_axis_4, 'c-', label='mosalingua+fr')
+    plt.plot_date(x_axis, y_axis_5, 'b-', label='other')
+    plt.plot_date(x_axis, y_axis_6, 'y-', label='program')
+    plt.plot_date(x_axis, y_axis_7, 'k-', label='shmoop_left')
+    plt.plot_date(x_axis, y_axis_8, 'm-', label='twitter')
+
+    plt.text(Timestamp('2016-02-29 00:00:00'), 0.65625, 'twitter')
+    plt.text(Timestamp('2016-02-29 00:00:00'), 0.611111111111, 'shmoop_left')
+    plt.text(Timestamp('2016-02-29 00:00:00'), 0.554945054945, 'answers')
+    plt.text(Timestamp('2016-02-29 00:00:00'), 0.543766578249, 'biznesowe+rewolucje')
+    plt.text(Timestamp('2016-02-29 00:00:00'), 0.525, 'mosalingua+fr')
+    plt.text(Timestamp('2016-02-29 00:00:00'), 0.467741935484, 'program')
+    plt.text(Timestamp('2016-02-29 00:00:00'), 0.4375, 'handbook')
+    plt.text(Timestamp('2016-02-29 00:00:00'), 0.240479916536, 'other')
+
+    plt.gcf().autofmt_xdate()
+    plt.show()
 
     return
 
